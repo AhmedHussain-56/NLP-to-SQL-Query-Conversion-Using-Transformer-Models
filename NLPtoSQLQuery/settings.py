7b9bcccf0d446a90a6d61b90daca624ed014ef88
Ahmed Hussain <ahmedhns654@gmail.com>
2026-01-29 01:06:18 +0530

    Fix: Use CompressedStaticFilesStorage to avoid missing source map errors

--- NLPtoSQLQuery/settings.py
+++ NLPtoSQLQuery/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets/static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
